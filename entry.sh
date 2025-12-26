a749c926aa25052a14ffb7f51f328fd76252807d
sidey79 <7968127+sidey79@users.noreply.github.com>
2025-12-26 19:53:09 +0100

    fix: vars from env

--- entry.sh
+++ entry.sh
@@ -4,9 +4,9 @@ mkdir -p /llm/ollama
 cd /llm/ollama
 
 init-ollama
-export OLLAMA_NUM_GPU=999
-export ZES_ENABLE_SYSMAN=1
-export GGML_SYCL_DEBUG=1
+
+source /opt/intel/oneapi/setvars.sh
+
 exec ollama serve
 
 # start ollama service
